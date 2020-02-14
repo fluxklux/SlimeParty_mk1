078107dc9b74365fa12b347878e42609d8426a36
Alexander Vikberg <downtobusinessalex@gmail.com>
2020-02-14 14:04:48 +0100

    Created UIController, updated GameController

--- Unity/Assets/Scripts/GameController.cs
+++ Unity/Assets/Scripts/GameController.cs
@@ -7,4 +7,29 @@ public class GameController : MonoBehaviour
     public GameObject[] allSlots;
     public Slot[] allSlotTypes;
 
+    int[] playerFruits = { 0, 0, 0, 0 };
+
+    UIController uc;
+
+    private void Start()
+    {
+        uc = GetComponent<UIController>();
+
+        for (int i = 0; i < allSlots.Length; i++)
+        {
+            allSlots[i].GetComponentInChildren<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    public void ChangeFruitAmount(int playerIndex, int amount)
+    {
+        playerFruits[playerIndex] += amount;
+
+        if (playerFruits[playerIndex] < 0)
+        {
+            playerFruits[playerIndex] = 0;
+        }
+        uc.UpdatePlayerFruits(playerFruits);
+    }
+
 }
---
+++ Unity/Assets/Scripts/UIController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+
+    public GameObject[] playerTexts;
+
+    public void UpdatePlayerFruits(int[] playerFruits)
+    {
+        for (int i = 0; i < playerTexts.Length; i++)
+        {
+            //Väntar på Canvas Element
+            //playerTexts[i].GetComponentInChildren<Text>().text = "P" + i + ": " + playerFruits[i];
+        }
+    }
+}
