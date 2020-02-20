91a0d18a3f5e8b4fcc076b9d00f7cb1ae0de90ec
Carl Eriksson <carl.erikssonn@hotmail.com>
2020-02-20 10:26:36 +0100

    Added mash potato minigame

---
+++ Unity/Assets/Scripts/Minigame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum MinigameType { Masher, Reaction }
+
+[CreateAssetMenu(fileName = " new Minigame", menuName = "Custom/Minigame")]
+public class Minigame : ScriptableObject
+{
+    public int index;
+    public MinigameType minigameType;
+    public float time;
+}
