a6d80136f325891e4680743843de63e85d18ac50
Alexander <downtobusinessalex@gmail.com>
2020-03-06 11:46:02 +0100

    Added RoundController

---
+++ Unity/Assets/Scripts/RoundController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundController : MonoBehaviour
+{
+    private int round;
+
+    public int roundMax;
+
+    void Start()
+    {
+        round = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateRound()
+    {
+        round++;
+        if(round >= roundMax)
+        {
+            Time.timeScale = 0f;
+        }
+    }
+}
