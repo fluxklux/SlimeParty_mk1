ed9e2ff83c5a04681da529e5d83a883a34a1d340
Alexander <downtobusinessalex@gmail.com>
2020-03-06 09:45:08 +0100

    Added Sparkle Effect Prefab

--- Unity/Assets/Scripts/GameController.cs
+++ Unity/Assets/Scripts/GameController.cs
@@ -14,6 +14,7 @@ public class GameController : MonoBehaviour
     public GameObject[] allSlots;
     public Slot[] allSlotTypes;
 
+    
     public List<QueueObject> queueObjects = new List<QueueObject>();
 
     int[] playerFruits = { 0, 0, 0, 0 };
@@ -87,6 +88,7 @@ public class GameController : MonoBehaviour
     public void ChangeFruitAmount(int playerIndex, int amount)
     {
         playerFruits[playerIndex] += amount;
+
 
         if (playerFruits[playerIndex] < 0)
         {
